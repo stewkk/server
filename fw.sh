4d8eddb35972b5e808d526e000f8b39dc868284d
Starovoytov Alexandr <saniy200311@gmail.com>
2022-05-15 09:16:53 +0000

    IDK

--- fw.sh
+++ fw.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-ufw-docker delete allow swag 80/tcp
-ufw-docker delete allow swag 443/tcp
-ufw-docker delete allow transmission 51413/tcp
-ufw-docker delete allow transmission 51413/udp
+/usr/local/bin/ufw-docker delete allow swag 80/tcp
+/usr/local/bin/ufw-docker delete allow swag 443/tcp
+/usr/local/bin/ufw-docker delete allow transmission 51413/tcp
+/usr/local/bin/ufw-docker delete allow transmission 51413/udp
+/usr/local/bin/ufw-docker delete allow gitea 222
 
-ufw-docker allow swag 80/tcp
-ufw-docker allow swag 443/tcp
-ufw-docker allow transmission 51413/tcp
-ufw-docker allow transmission 51413/udp
+/usr/local/bin/ufw-docker allow swag 80/tcp
+/usr/local/bin/ufw-docker allow swag 443/tcp
+/usr/local/bin/ufw-docker allow transmission 51413/tcp
+/usr/local/bin/ufw-docker allow transmission 51413/udp
+/usr/local/bin/ufw-docker allow gitea 222
 
-ufw reload
+/usr/sbin/ufw reload
 
